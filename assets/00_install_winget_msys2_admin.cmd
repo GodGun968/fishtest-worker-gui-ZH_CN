d185798c64008fe4e9f096286e050a13cd6f2ee0
神枪968 <67826085+GodGun968@users.noreply.github.com>
2026-09-10 18:01:21 +0800

    Add files via upload

--- assets/00_install_winget_msys2_admin.cmd
+++ assets/00_install_winget_msys2_admin.cmd
@@ -3,8 +3,8 @@ chcp 65001 >nul
 :: 如果缺少 winget，则安装它
 :: https://learn.microsoft.com/en-us/windows/package-manager/winget/
 where winget >nul 2>nul || (
-    echo 正在以静默模式安装 winget，这需要一些时间...
-    powershell -Command "$progressPreference = 'silentlyContinue'; Write-Information '正在下载 WinGet 及其依赖项...'; Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx; Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx; Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx; Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx; Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx; Remove-Item Microsoft.UI.Xaml.2.8.x64.appx"
+    echo 正在以静默模式安装 winget，这需要一些时间……
+    powershell -Command "$progressPreference = 'silentlyContinue'; Write-Information '正在下载 WinGet 及其依赖项……'; Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx; Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx; Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx; Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx; Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx; Remove-Item Microsoft.UI.Xaml.2.8.x64.appx"
 )
 
 :: 检查是否已经安装 MSYS2
