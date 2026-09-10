fb969731db8e728dd746027aca5d1f347679ec36
神枪968 <67826085+GodGun968@users.noreply.github.com>
2026-09-10 23:00:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from i18n import get_language, language_name, localized_level, set_language, sup
 
 # --- 常量 ---
 APP_NAME = "Fishtest Worker Manager I18N"
-APP_VERSION = "v0.0.1"
+APP_VERSION = "v0.0.3"
 REPO_OWNER = "GodGun968"
 REPO_NAME = "fishtest-worker-gui-I18N"
 
