3179724c49706db444bc43ddd1138b5d8fded40c
神枪968 <67826085+GodGun968@users.noreply.github.com>
2026-09-10 22:26:47 +0800

    Update i18n.py

--- i18n.py
+++ i18n.py
@@ -27,7 +27,7 @@ LOG_LEVEL_NAMES = {
 
 TRANSLATIONS = {
     "zh_CN": {
-        "app.window_title": "Fishtest Worker 管理器 ({version})",
+        "app.window_title": "Fishtest Worker 管理器 I18N ({version})",
         "button.install": "安装/重新安装 Worker",
         "button.update_msys2": "更新 MSYS2 环境",
         "button.settings": "设置",
